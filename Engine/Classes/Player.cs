88487f9153acb3dee72135d32fef0c6b8ebf05cd
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-07-09 11:53:24 +0200

    Projektdateien hinzufügen.

---
+++ Engine/Classes/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Classes
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int Hitpoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
